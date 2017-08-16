42f40eab051c501365d0eaa13374025a155cc8a5
SG Wang <wsg20110828@163.com>
2017-08-16 17:18:57 +0800

    titanic to 79.9%

--- titanic/Random-Forest.py
+++ titanic/Random-Forest.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import accuracy_score
 
 print "load data..."
 train = pd.read_csv("data/train.csv")
@@ -13,6 +17,8 @@ print "clean data..."
 
 
 def clean_data(titanic):
+    titanic["child"] = (titanic["Age"] < 14)
+
     titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
 
     titanic.loc[titanic["Sex"] == "male", "Sex"] = 0
@@ -23,30 +29,46 @@ def clean_data(titanic):
     titanic.loc[titanic["Embarked"] == 'Q', "Embarked"] = 3
     titanic.loc[titanic["Embarked"].isnull()] = 1
 
+    titanic["family_num"] = titanic["SibSp"] + titanic["Parch"]
+
     titanic["Fare"] = titanic["Fare"].fillna(titanic["Fare"].median())
 
     return titanic
 
 train_data = clean_data(train)
-test_data = clean_data(train)
+test_data = clean_data(test)
 
 # Engineer Features
 print "Engineer Feature..."
-predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
-
+#predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "child",
+#              "family_num"]
+predictors = ["Pclass", "Sex","Age", "SibSp", "Parch", "Fare", "Embarked", "child", "family_num"]
+print "features:", predictors
 # Model of Random Forest
 print "fit model..."
 rf = RandomForestClassifier(
-    n_estimators=100,
-    min_samples_split=4,
-    min_samples_leaf=2,
+    #n_estimators=200,
+    max_depth=4,
+    min_samples_split=6,
+    min_samples_leaf=6,
     oob_score=True
 )
+param_test = {
+    'n_estimators': range(150, 220, 10)
+    #'min_samples_split': range(5, 10, 1),
+    #'min_samples_leaf': range(4, 10, 2)
+    #'max_depth': range(2, 8, 2)
+}
+clf = GridSearchCV(estimator=rf, param_grid=param_test, scoring='accuracy', cv=10)
+clf.fit(train_data[predictors], train_data["Survived"])
 
-rf.fit(train_data[predictors], train_data["Survived"])
-print rf.oob_score
+print(clf.grid_scores_, clf.best_params_, clf.best_score_)
 
-predict_data = rf.predict(test_data[predictors])
+predict_data = clf.predict(test_data[predictors])
 
+submission = pd.DataFrame({
+    "PassengerId": test["PassengerId"],
+    "Survived": predict_data
+})
 
-
+submission.to_csv('data/random-forest.csv', index=False)
